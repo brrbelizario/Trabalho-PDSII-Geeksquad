82430d4dcde6d0665db73cf4ca262b325cb4c05b
Brenda <brenda0belizario@gmail.com>
2025-01-30 18:11:23 -0300

    main corrigida

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,22 +9,22 @@
 #include <ctime>
 #include <algorithm>      
 
-#include "Usuario.cpp"
-#include "Carteira.cpp"
-#include "Transacao.cpp"
-#include "Criptomoeda.cpp"
-#include "Historico.cpp"
-#include "Logger.cpp"
-#include "Cadastro.cpp"
-#include "Login.cpp"
-#include "Transferencia.cpp"
-#include "Saldo.cpp"
-#include "Deposito.cpp"
-#include "Retirada.cpp"
-#include "Notificacao.cpp"
-#include "Validador.cpp"
-#include "Relatorio.cpp"
-#include "Sistema.cpp"
+#include "../include/Usuario.hpp"
+#include "../include/Carteira.hpp"
+#include "../include/Transacao.hpp"
+#include "../include/Criptomoeda.hpp"
+#include "../include/Historico.hpp"
+#include "../include/Logger.hpp"
+#include "../include/Cadastro.hpp"
+#include "../include/Login.hpp"
+#include "../include/Transferencia.hpp"
+#include "../include/Saldo.hpp"
+#include "../include/Deposito.hpp"
+#include "../include/Retirada.hpp"
+#include "../include/Notificacao.hpp"
+#include "../include/Validador.hpp"
+#include "../include/Relatorio.hpp"
+#include "../include/Sistema.hpp"
 
 using namespace std;
 
